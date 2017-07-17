e617ebda19355afb06b5d03a49b60969640eda5a
Marty Bonacci <martybonacci@gmail.com>
2017-07-17 16:50:43 -0600

    edited the elevator pitch

--- index.php
+++ index.php
@@ -31,7 +31,16 @@
 		</ol>
 		<br/>
 		<h2>Elevator Pitch</h2>
-		<p>I believe that we are unique, with unique bodies, thoughts, feelings and identities. People go to great lengths to celebrate their individuality. Is your company actively pursuing this market?</p>
+		<p>I believe that we are unique, with unique bodies, thoughts, feelings and identities.
+			People go to great lengths to celebrate their individuality.
+			I help companies profit from new mass-customization technologies.
+			Is your company actively pursuing this market?</p>
+		<p>Dannielle (my buddy) thinks my elevator pitch should be like this:</p>
+		<p>Hi my name is Marty I believe in being unique in all aspects of our lives.
+			By treating everyone as a unique individual, I am able to help make them custom products.
+			People deserve to have/use/wear products that celebrate the fact that they are one of a kind.
+			Do like when you are able to express yourself?</p>
+		<p>I like aspects of it and want to kick it around a bit.</p>
 		<br/>
 		<h2>My Learning Style</h2>
 		<p><strong>Visual</strong></p>
