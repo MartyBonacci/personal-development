2ce0b4f936a57eb952a549db78d3d238267542d4
Marty Bonacci <martybonacci@gmail.com>
2017-07-17 16:19:49 -0600

    created index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Marty Bonacci">
+		<meta name="description" content="Personal Development">
+		<title>Personal Development</title>
+	</head>
+	<body>
+		<h1>Marty Bonacci's Personal Development</h1>
+		<br/>
+		<h2>Quantitative Accomplishments</h2>
+		<ul>
+			<li>Over 20 years of mass-customization system development</li>
+			<li>Three ski/snowboard companies use these systems to produce premium bespoke products with production line efficiency.</li>
+			<li>Marty has two patents pending on mass-customization automation systems.</li>
+		</ul>
+		<br/>
+		<h2>Qualitative Accomplishments</h2>
+		<ul>
+			<li>Developed mass-customization selling, engineering, and manufacturing systems to automate the process of providing customer unique products</li>
+			<li>Created software to skip the product engineering/design phase and go directly to component CNC machining</li>
+			<li>Designed and build entire ski and snowboard production lines, including machines and tooling for mass-customization</li>
+		</ul>
+		<br/>
+		<h2>Golden Circle</h2>
+		<ol>
+			<li>Why? I believe that everyone is unique, with a unique body, thoughts, feeling and identity.</li>
+			<li>How? You deserve to be treated as a unique individual.</li>
+			<li>What? You deserve to have/use/wear products that celebrate the fact that you are one of a kind.</li>
+		</ol>
+		<br/>
+		<h2>Elevator Pitch</h2>
+		<p>I believe that we are unique, with unique bodies, thoughts, feelings and identities. People go to great lengths to celebrate their individuality. Is your company actively pursuing this market?</p>
+		<br/>
+		<br/>
+
+		<p>My bootcamp buddy is Dannielle Bojorquez.</p>
+
+	</body>
+</html>
